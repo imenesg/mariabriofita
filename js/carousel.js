4b7f46cfaf8f61e96f2d41c6e384db4576a1273e
imenesg <imenesgabriel@gmail.com>
2022-03-06 23:46:01 -0300

    first commit

---
+++ js/carousel.js
@@ -0,0 +1,7 @@
+/* O js foi inportado seguindo o link : https://css-tricks.com/creating-responsive-touch-friendly-carousels-with-flickity/ */
+
+var flkty = new Flickity( '.main-gallery', {
+    // options
+    cellAlign: 'left',
+    contain: true
+  }); 
