702002b8259bdfa5c62ea0bf84e937e5ea62d77b
imenesg <imenesgabriel@gmail.com>
2022-03-07 19:40:22 -0300

    add maria foto.

--- js/carousel.js
+++
@@ -1,7 +0,0 @@
-/* O js foi inportado seguindo o link : https://css-tricks.com/creating-responsive-touch-friendly-carousels-with-flickity/ */
-
-var flkty = new Flickity( '.main-gallery', {
-    // options
-    cellAlign: 'left',
-    contain: true
-  }); 
